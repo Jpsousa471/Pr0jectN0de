fc583439a9ea72cccce27b1fa9a5d5491ea2a20a
João Pedro <jpsousa471@gmail.com>
2024-09-19 20:18:19 -0300

    continuando

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 
 import express from 'express';
-
-
+import { pool } from './mysql';
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 
@@ -11,9 +11,22 @@ app.get('/users', (request, response) => {
     response.json([{name: 'João', age:20},{name: 'Pedro', age:22}]);
 })
 
-app.post('/userdata', (request, response) => {
-    console.log(request.body)
-    response.status(200).json({sucess: true})
+app.post('/user', (request, response) => {
+   const {name, email, password} = request.body;
+   pool.getConnection((err: any, connection: any) => {
+    connection.query(
+        'INSERT INTO users (user_id, name, email, password) VALUES (?,?,?,?)',
+        [uuidv4(), name, email, password],
+        (error: any, result: any, fildes: any) => {
+            if (error) {
+                return response.status(400).json(error)
+            }
+
+            return response.status(200).json({succes: true});
+
+        }
+    )
+   })
 })
 
 
